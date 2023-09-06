ed8d9588b708395db5e5c5cf789227db2f06fc6c
edchavezb <edgp.chavez@gmail.com>
2023-09-05 20:46:21 -0600

    Add Tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#300d38',
+        secondary: '#899975',
+        tertiary: '#ffa900',
+        text: '#090325',
+        background: '#f9f7f2',
+        cream: '#ffdfb1'        
+      },
+      boxShadow: {
+        card: 'rgba(0, 0, 0, 0.15) 0px 0px 20px 0px'
+      },
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
